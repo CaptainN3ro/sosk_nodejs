1ff249b8a37a5751dfe9cbfe6522bc891f695879
CaptainN3ro <nicolas-schwarz@outlook.de>
2023-11-03 18:18:55 +0100

    Added static frontend

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const cors = require('cors');
 require("dotenv/config");
 
 const app = express();
 
 app.use(bodyParser.json({ limit: "30mb", type: "application/json" }));
+app.use(cors());
 
 app.get("/helloFriend", (req, res) => {
   res.send("Hallo Freund");
@@ -42,6 +44,8 @@ app.post("/insert", async (req, res) => {
     res.status(500).send({ error: e });
   }
 });
+
+app.use(express.static('frontend'));
 
 mongoose.connect(process.env.DB_CONNECTION);
 
